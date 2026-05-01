8bfaf013dcc2ecad44c9df0e2058c07e10a7d3e6
Louis GUICHARD (glpda) <git@glpda.net>
2026-05-01 22:32:28 +0200

    Add completion for hex retirement reason

--- gleam.fish
+++ gleam.fish
@@ -147,11 +147,12 @@ complete -c gleam -n '__fish_prev_arg_in hex' -a authenticate -d "Authenticate w
 complete -c gleam -n '__fish_prev_arg_in hex' -a owner -d "Deal with package ownership"
 complete -c gleam -n '__fish_prev_arg_in owner' -a transfer -d "Give package ownership to a new Hex user"
 complete -c gleam -n '__fish_seen_subcommand_from transfer' -l to -rf -d 'The username or email of the new owner'
-complete -c gleam -n '__fish_prev_arg_in hex' -a retire -d "Retire a release from Hex"
+complete -c gleam -n '__fish_prev_arg_in hex' -a retire   -d "Retire a release from Hex"
 complete -c gleam -n '__fish_prev_arg_in hex' -a unretire -d "Un-retire a release from Hex"
 complete -c gleam -n '__fish_prev_arg_in hex' -a revert   -d "Revert a release from Hex"
 complete -c gleam -n '__fish_seen_subcommand_from retire unretire revert transfer' -l package -rf -a '(__fish_gleam_hex_packages)'
 complete -c gleam -n '__fish_seen_subcommand_from retire unretire revert' -l version -rf
+complete -c gleam -n '__fish_seen_subcommand_from retire' -l reason -xa 'other invalid security deprecated renamed'
 complete -c gleam -n '__fish_seen_subcommand_from publish; and not __fish_seen_subcommand_from docs' -l replace
 complete -c gleam -n '__fish_seen_subcommand_from publish; and not __fish_seen_subcommand_from docs' -s y -l yes
 
